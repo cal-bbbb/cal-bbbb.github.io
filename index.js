90d70cd90c117a77cdb06ca0e57a7fb22bff3f34
cal-bbbb <119997413+cal-bbbb@users.noreply.github.com>
2022-12-07 00:23:46 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>Welcome to my website!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));

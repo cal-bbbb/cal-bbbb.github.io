4b25c94f3157f2c661c5cb51ab432bcb1d783a8b
cal-bbbb <119997413+cal-bbbb@users.noreply.github.com>
2023-08-18 16:10:07 -0400

    for da ai

--- projects/projectCards.js
+++ projects/projectCards.js
@@ -2,7 +2,8 @@
 const codeProjectsDataPromise = d3.json("/resources/code-projects.json");
 const designProjectsDataPromise = d3.json("/resources/design-projects.json");
 
-Promise.all([codeProjectsDataPromise, designProjectsDataPromise]).then(([codeProjects, designProjects]) => {
+Promise.all([codeProjectsDataPromise, designProjectsDataPromise]).then(
+  ([codeProjects, designProjects]) => {
     // Combine the projects
     const allProjects = [...codeProjects, ...designProjects];
 
@@ -16,42 +17,66 @@ Promise.all([codeProjectsDataPromise, designProjectsDataPromise]).then(([codePro
     // Get the top 3 latest projects for the featured section
     const featuredProjects = sortedProjects.slice(0, 3);
     populateProjectsSection(featuredProjects, "featured-projects", true);
-});
+  }
+);
 
 function populateProjectsSection(data, sectionId, isFeatured = false) {
-    const section = d3.select(`#${sectionId}`);
+  const section = d3.select(`#${sectionId}`);
 
-    data.forEach((project) => {
-        const projectCard = section.append("div").attr("class", isFeatured ? "project-card featured" : "project-card");
-        
-        // Create an anchor element for the project link
-        const projectLink = projectCard.append("a").attr("href", project["project-link"]);
+  data.forEach((project) => {
+    const projectCard = section
+      .append("div")
+      .attr("class", isFeatured ? "project-card featured" : "project-card");
 
-        // Card content container with the body-text class
-        const cardContent = projectLink.append("div").attr("class", "project-card-content body-text");
+    // Create an anchor element for the project link
+    const projectLink = projectCard
+      .append("a")
+      .attr("href", project["project-link"]);
 
-        // Add project title and description on the left
-        const textContainer = cardContent.append("div").attr("class", "project-text-container");
-        textContainer.append("h4").attr("class", "project-card-title").text(project.title);
-        textContainer.append("p").attr("class", "project-card-description").text(project.description);
+    // Add project image on top (if exists)
+    if (project.file_name) {
+      projectLink
+        .append("div")
+        .attr("class", "project-card-preview")
+        .append("img")
+        .attr(
+          "src",
+          `/projects/${project["project-link"]}/images/${project.file_name}`
+        );
+    }
+    // Card content container with the body-text class
+    const cardContent = projectLink
+      .append("div")
+      .attr("class", "project-card-content body-text");
 
-        // Add project image on the right (if exists)
-        if (project.file_name) {
-            projectLink.append("div")
-                .attr("class", "project-card-preview")
-                .append("img")
-                .attr("src", `/projects/${project["project-link"]}/images/${project.file_name}`);
-        }
+    // Add project title and description on the bottom
+    const textContainer = cardContent
+      .append("div")
+      .attr("class", "project-text-container");
+    textContainer
+      .append("h4")
+      .attr("class", "project-card-title")
+      .text(project.title);
+    textContainer
+      .append("p")
+      .attr("class", "project-card-description")
+      .text(
+        project.description.substring(0, 85) +
+          (project.description.length > 85 ? "..." : "")
+      );
 
-        // Add tags (if exists)
-        if (project.tags && project.tags.length) {
-            const tagsDiv = textContainer.append("div").attr("class", "tags-container");
-            project.tags.forEach((tag) => {
-                tagsDiv.append("div")
-                    .attr("class", "tag")
-                    .attr("data-tag", tag)
-                    .text(tag);
-            });
-        }
-    });
+    // Add tags (if exists)
+    if (project.tags && project.tags.length) {
+      const tagsDiv = textContainer
+        .append("div")
+        .attr("class", "tags-container");
+      project.tags.forEach((tag) => {
+        tagsDiv
+          .append("div")
+          .attr("class", "tag")
+          .attr("data-tag", tag)
+          .text(tag);
+      });
+    }
+  });
 }

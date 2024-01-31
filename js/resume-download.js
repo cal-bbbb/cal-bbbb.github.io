62aa0191007a1ea6894c801934009c98b28e53ec
cal-bbbb <119997413+cal-bbbb@users.noreply.github.com>
2024-01-31 12:11:51 -0500

    new resume

--- js/resume-download.js
+++ js/resume-download.js
@@ -1,6 +1,6 @@
 function downloadResume() {
   const link = document.createElement("a");
-  link.href = "/resources/cboone_resume_2024.pdf";
+  link.href = "/resources/resume_2024.pdf";
   link.download = "resume_current.pdf";
   link.dispatchEvent(new MouseEvent("click"));
 }
